56b7a8e10de3fff9e8235e90a77da4b94d31def6
appiciaindia <appiciaindia@gmail.com>
2026-07-17 17:28:29 +0530

    chng

--- src/app/api/pdf/[id]/route.js
+++ src/app/api/pdf/[id]/route.js
@@ -4,12 +4,11 @@ import Quotation from "../../../../component/quotation/quotation";
 export async function GET(req, { params }) {
   const { id } = await params;
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/invoice/${id}`,
-    {
-      cache: "no-store",
-    }
-  );
+const url = new URL(`/api/invoice/${id}`, req.url);
+
+const res = await fetch(url, {
+  cache: "no-store",
+});
 
   const data = await res.json();
 
